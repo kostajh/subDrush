8879ff370836756bc7731567be5dfcaa17b22196
Kosta Harlan <kostajh@gmail.com>
2013-05-06 13:50:00 -0400

    Beginning of watchdog show command

--- sublime-drush.py
+++ sublime-drush.py
@@ -220,6 +220,26 @@ class DrushDownloadCommand (sublime_plugin.WindowCommand):
         sublime.status_message("Downloaded '%s' to %s" % (self.args[idx][1], drush_api.get_drupal_root()))
 
 
+class DrushWatchdogShowCommand (sublime_plugin.WindowCommand):
+    def run(self):
+        self.panel_name = 'watchdog'
+        self.window.create_output_panel(self.panel_name)
+        self.panel = self.window.get_output_panel('watchdog')
+        self.view = self.window.active_view()
+        # self.window.run_command("show_panel", {"panel": "output.%s" % self.panel_name})
+        test = 'hello'
+        self.panel.run_command('drush_watchdog_show_output', {"panel": "output.%s" % test})
+        print('run')
+    def on_done(self):
+        print('done')
+
+class DrushWatchdogShowOutputCommand (sublime_plugin.TextCommand):
+    def run(self, edit, output):
+        print('test')
+        self.view.insert(edit, self.view.size(), output)
+        print('running')
+
+
 class SublimeDrushCacheClearCommand (sublime_plugin.WindowCommand):
     def run(self):
         print('clear')
