6cdc7c68e80950818c7031c79344c47d606acb33
Kosta Harlan <kostajh@gmail.com>
2013-06-03 15:23:39 -0400

    Thread watchdog show command

--- commands/drush_watchdog_show_command.py
+++ commands/drush_watchdog_show_command.py
@@ -10,16 +10,31 @@ class DrushWatchdogShowCommand(sublime_plugin.WindowCommand):
     A command to display Drupal's watchdog log.
     """
     def run(self):
+        sublime.status_message('Loading watchdog entries...')
+        thread = DrushWatchdogShowThread(self.window)
+        thread.start()
+
+
+class DrushWatchdogShowThread(threading.Thread):
+    """
+    A thread to return watchdog entries.
+    """
+    def __init__(self, window):
+        self.window = window
+        threading.Thread.__init__(self)
+
+    def run(self):
+        drush_api = DrushAPI()
         self.view = self.window.active_view()
-        window = self.view.window()
-        if window:
-            self.drush_api = DrushAPI()
-            working_dir = self.view.window().folders()
-            self.drush_api.set_working_dir(working_dir[0])
-            watchdog = self.drush_api.run_command('watchdog-show',
-                                                  '')
-            if watchdog:
-                output = window.create_output_panel("watchdog")
-                output.run_command('erase_view')
-                output.run_command('insert_view', {'string': watchdog})
-                window.run_command("show_panel", {"panel": "output.watchdog"})
+        working_dir = self.view.window().folders()
+        drush_api.set_working_dir(working_dir[0])
+        watchdog = drush_api.run_command('watchdog-show', '')
+        if not watchdog:
+            sublime.status_message('Could not find a Drupal directory')
+            return
+        sublime.status_message('Loaded 10 most recent watchdog entries')
+        if watchdog:
+            output = self.window.create_output_panel("watchdog")
+            output.run_command('erase_view')
+            output.run_command('insert_view', {'string': watchdog})
+            self.window.run_command("show_panel", {"panel": "output.watchdog"})
