36492f878256c3146a965c6617b293f7b963cc10
Kosta Harlan <kostajh@gmail.com>
2013-06-11 14:27:04 -0400

    Fix definition of on_load_async().

--- subDrush.py
+++ subDrush.py
@@ -6,6 +6,6 @@ from .lib.drush import DrushAPI
 
 class SublimeDrush(sublime_plugin.EventListener):
 
-    def on_load_async(self):
+    def on_load_async():
         drush_api = DrushAPI()
         drush_api.load_command_args('core-status')
