14d421810b86aff0c15ac8eea88a8f8f5b5ee70c
Kosta Harlan <kostajh@gmail.com>
2013-06-11 14:58:35 -0400

    Update PHP Script command name for import

--- commands/__init__.py
+++ commands/__init__.py
@@ -3,7 +3,7 @@ from .drush_cache_clear_all_command import DrushCacheClearAllCommand
 from .drush_variable_get_command import DrushVariableGetCommand
 from .drush_watchdog_show_command import DrushWatchdogShowCommand
 from .drush_alias_edit_command import DrushAliasEditCommand
-from .drush_php_script import DrushPhpScriptCommand
+from .drush_php_script_command import DrushPhpScriptCommand
 from .drush_eval_command import DrushEvalCommand
 from .drush_cron_command import DrushCronCommand
 from .sublime_drush_cache_clear_command import SublimeDrushCacheClearCommand
