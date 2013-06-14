fba0840a6fc130333df4e9711b9576e8d0292a16
Kosta Harlan <kostajh@gmail.com>
2013-06-14 10:38:33 -0400

    Use YAML for watchdog-show command

--- commands/drush_watchdog_show_command.py
+++ commands/drush_watchdog_show_command.py
@@ -28,14 +28,17 @@ class DrushWatchdogShowThread(threading.Thread):
 
     def run(self):
         drush_api = DrushAPI(self.window.active_view())
-        watchdog = drush_api.run_command('watchdog-show', list(), list())
+        options = list()
+        options.append('--format=yaml')
+        options.append('--full')
+        watchdog = drush_api.run_command('watchdog-show', list(), options)
         if not watchdog:
             sublime.status_message('Could not find a Drupal directory')
             return
         sublime.status_message('Loaded 10 most recent watchdog entries')
         if watchdog:
             output = self.window.create_output_panel("watchdog")
-            output.set_syntax_file("Packages/Text/Plain Text.tmLanguage")
+            output.set_syntax_file("Packages/YAML/YAML.tmLanguage")
             output.run_command('erase_view')
             output.run_command('append', {'characters': watchdog})
             self.window.run_command("show_panel", {"panel": "output.watchdog"})
